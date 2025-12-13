065c3bafb73021b0b82e22131cbb4fabbd8bf75b
CommitTheKermit <qnf323@naver.com>
2025-12-14 02:25:01 +0900

    fix(output): 개행 오류 수정

--- src/main/kotlin/store/constants/Messages.kt
+++ src/main/kotlin/store/constants/Messages.kt
@@ -4,5 +4,5 @@ object Messages {
     const val WELCOME_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n"
     const val SHOPPING_GUIDE = "\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n"
     const val MEMBERSHIP = "멤버십 할인을 받으시겠습니까? (Y/N)"
-    const val BUY_ANOTHER_GUIDE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)"
+    const val BUY_ANOTHER_GUIDE = "\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)"
 }
--- src/main/kotlin/store/view/InputView.kt
+++ src/main/kotlin/store/view/InputView.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 object InputView {
     fun readLine(): String {
         val input = Console.readLine()
-
+        println()
         return input ?: ""
     }
 
